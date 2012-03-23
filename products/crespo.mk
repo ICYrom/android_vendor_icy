8a08932b28ab02e9504641be692b24e9f12ffe8c
ICYrom <drewzamanigian@gmail.com>
2012-03-23 03:17:04 -0400

    Updated

--- products/crespo.mk
+++ products/crespo.mk
@@ -24,12 +24,15 @@ PRODUCT_COPY_FILES +=  \
     vendor/icy/proprietary/crespo/app/Thinkfree.apk:system/app/Thinkfree.apk \
     vendor/icy/proprietary/crespo/media/bootanimation.zip:system/media/bootanimation.zip \
     vendor/icy/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/icy/proprietary/common/app/FileManager.apk:system/app/FileManager.apk \
     vendor/icy/proprietary/common/app/Microbes.apk:system/app/Microbes.apk \
     vendor/icy/proprietary/common/app/Wallet.apk:system/app/Wallet.apk \
+    vendor/icy/proprietary/common/app/TouchWiz30LauncherICS.apk:system/app/TouchWiz30LauncherICS.apk \
     vendor/icy/proprietary/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
     vendor/icy/proprietary/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
     vendor/icy/proprietary/common/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
     vendor/icy/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
+    vendor/icy/proprietary/common/lib/libglcanvas.so:system/lib/libglcanvas.so \
     vendor/icy/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/icy/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
     
